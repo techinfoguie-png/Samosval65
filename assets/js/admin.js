97b50fcc2c48ef23dbb63051c0c5bed1aa4ce164
techinfoguie-png <techinfoguie@gmail.com>
2026-05-20 23:34:43 +1100

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,7 +5,7 @@
   // 🔧 1. КОНФИГУРАЦИЯ (всегда самая первая!)
   const CFG = {
     CMS_URL: 'https://script.google.com/macros/s/AKfycbzk4T_aLnTlEq-q5v4rGqFWtENTEvSvZ1wqOyKKMLBuh0HG6rwxkbYognua149x9Hze3Q/exec', // ← URL из Apps Script
-    PASSWORD: 'Samosval65!@#', // ← Пароль для входа
+    PASSWORD: 'Samosval65', // ← Пароль для входа
     SHEET_URL: 'https://docs.google.com/spreadsheets/d/17bbtzt_kGkVFlWZeVTP0yLvWFT0kies9qEdPTjm66NE/edit' // ← Ссылка на таблицу
   };
 
